0d59f5647394ab57f63c114a3fc66fb70c3e7371
alok <alok.aggarwal@sysdig.com>
2022-08-18 15:10:11 -0700

    some renames

---
+++ code/pass_fd_across_namesspaces/setns_sock_fd.cpp
@@ -0,0 +1,65 @@
+#include <sched.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+// 1. Setns to host's mount ns
+// 2. open a directory and get fd
+// 3. setns back to container's mount ns
+// 4. create a file relative to the fd from host 
+int main()
+{
+	int err = 0, dirfd, fd, host_mnt_fd, container_mnt_fd;
+	
+	host_mnt_fd = open("/proc/1/ns/mnt", O_RDONLY);
+	printf("host_mnt_fd = %d\n", host_mnt_fd);
+
+	err = setns(host_mnt_fd, CLONE_NEWNS);
+	if (err< 0) 
+	{
+		perror("\n Error: switching mount ns to host failed");
+	}
+	else
+	{
+		printf("setns to host successful\n");
+		unlink("/setns_exp/tmpfile");
+	}
+
+	dirfd = open("/setns_exp", O_PATH);
+
+	container_mnt_fd = open("/proc/self/ns/mnt", O_RDONLY);
+	printf("container__mnt_fd = %d\n", container_mnt_fd);
+
+	//Confirm that host mnt fd and container mnt fd are different
+	if (host_mnt_fd == container_mnt_fd)
+	{
+		printf("failure: container and host mount fds are same");
+		return 1;
+	}
+
+	err = setns(container_mnt_fd, CLONE_NEWNS);
+	if (err< 0) 
+	{
+		perror("\n Error: switching mount ns back to container failed");
+	}
+	else
+	{
+		printf("setns to container successful\n");
+	}
+
+    	fd = openat(dirfd, "tmpfile", O_CREAT, S_IXUSR);
+	if (fd>0)
+	{
+		printf("creating file in host ns successful\n");
+	}
+	else
+	{
+		perror("creating file in host ns not successful");
+	}
+	
+	close(fd);
+    	close(dirfd);
+
+}
